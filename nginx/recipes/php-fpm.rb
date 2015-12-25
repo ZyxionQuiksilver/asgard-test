e566a689c6f06aea59e27706479f7ea52b79afa1
zxq <Zyxionquiksilver@gmail.com>
2015-12-24 19:42:46 -0600

    phpinfo.php with cookbook files. still static. needs work.

--- nginx/recipes/php-fpm.rb
+++ nginx/recipes/php-fpm.rb
@@ -11,5 +11,42 @@ package "php5-fpm" do
 end
 
 service "php5-fpm" do
+	supports :restart => true, :reload => true
 	action [:enable, :start]
+end
+
+cookbook_file 	"/usr/share/nginx/html/phpinfo.php" do
+	source 		"phpinfo.php"
+	owner 		"root"
+	group 		"root"
+	mode 		"0644"
+	action		:create
+	notifies	:reload, "service[nginx]", :immediately
+end
+
+cookbook_file 	"/etc/nginx/sites-available/default" do
+	source 		"default"
+	owner 		"root"
+	group 		"root"
+	mode 		"0644"
+	action		:create
+	notifies	:reload, "service[nginx]", :immediately
+end
+
+cookbook_file	"/etc/php5/fpm/php.ini" do
+	source		"php.ini"
+	owner		"root"
+	group		"root"
+	mode		"0644"
+	action		:create
+	notifies	:reload, "service[php5-fpm]", :immediately
+end
+
+cookbook_file	"/etc/php5/fpm/pool.d/www.conf" do
+	source		"www.conf"
+	owner		"root"
+	group		"root"
+	mode		"0644"
+	action		:create
+	notifies	:reload, "service[php5-fpm]", :immediately
 end
