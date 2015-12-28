98690de0ac81137c2180da0c21f244f4aa8fb525
ZyxionQuiksilver <ZyxionQuiksilver@gmail.com>
2015-12-28 15:38:12 -0600

    git install w/ apt. needs to be a recipe.

--- chef-init.sh
+++ chef-init.sh
@@ -2,6 +2,8 @@
 cd ~
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.6.0-1_amd64.deb
 sudo dpkg -i chef_12.6.0-1_amd64.deb
-cd ~/.chef/
+sudo at-get install git -y
+mkdir asgard-test
 git clone https://github.com/ZyxionQuiksilver/asgard-test.git
-sudo chef-client -z -j 
+cd ~/asgard-test
+sudo chef-client -z -j recipes.json 
