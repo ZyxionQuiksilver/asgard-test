9996e6c752545562aef2918a24decf51780a349e
ZyxionQuiksilver <ZyxionQuiksilver@gmail.com>
2015-12-28 15:41:33 -0600

    typo... /sigh

--- chef-init.sh
+++ chef-init.sh
@@ -2,7 +2,7 @@
 cd ~
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.6.0-1_amd64.deb
 sudo dpkg -i chef_12.6.0-1_amd64.deb
-sudo at-get install git -y
+sudo apt-get install git -y
 mkdir asgard-test
 git clone https://github.com/ZyxionQuiksilver/asgard-test.git
 cd ~/asgard-test
