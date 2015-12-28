4be0673a0b8a921a63115720b60bafa2d90f2991
zxq <Zyxionquiksilver@gmail.com>
2015-12-28 15:14:44 -0600

    recipes json added

--- chef-init.sh
+++ chef-init.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-
+cd ~
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.6.0-1_amd64.deb
+sudo dpkg -i chef_12.6.0-1_amd64.deb
+cd ~/.chef/
+git clone https://github.com/ZyxionQuiksilver/asgard-test.git
+sudo chef-client -z -j 
