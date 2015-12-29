f56493e61036f5e94e8fc29e242fde05e9d5a202
ZyxionQuiksilver <ZyxionQuiksilver@gmail.com>
2015-12-29 16:58:29 -0600

    APT TIME!

--- apt-zxq/metadata.rb
+++ apt-zxq/metadata.rb
@@ -1,7 +1,16 @@
-name             'nginx'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures nginx'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+name 'apt'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Configures apt and apt services. Ships resources for managing apt repositories'
+issues_url 'https://github.com/chef-cookbooks/apt/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/apt' if respond_to?(:source_url)
+version '2.9.2'
+
+recipe 'apt::default', 'Runs apt-get update during compile phase and sets up preseed directories'
+recipe 'apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
+recipe 'apt::cacher-client', 'Client for the apt::cacher-ng caching proxy'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
